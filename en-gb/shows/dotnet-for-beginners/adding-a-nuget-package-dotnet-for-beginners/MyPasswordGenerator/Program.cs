bc246a187faa806edaf0bd50015fab6f88b81992
Ramu Mohan Srinivasan Radhakrishnan <ramumsrk02071981@gmail.com>
2024-02-16 10:42:31 +0530

    My attempt at the contents of https://learn.microsoft.com/en-gb/shows/dotnet-for-beginners/

---
+++ en-gb/shows/dotnet-for-beginners/adding-a-nuget-package-dotnet-for-beginners/MyPasswordGenerator/Program.cs
@@ -0,0 +1,7 @@
+﻿using PasswordGenerator;
+
+var password = new PasswordGenerator.Password();
+var anotherPassword = password.Next();
+System.Console.WriteLine(
+  $"Generated password is '{anotherPassword}'"
+);
---
+++ en-gb/shows/dotnet-for-beginners/installing-and-getting-started-with-dotnet-in-vs-code-dotnet-for-beginners/MyFirstConsoleApplication/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.Write("What is your name? ");
+var myName = System.Console.ReadLine();
+var todaysDate = DateTime.Now;
+System.Console.WriteLine(
+  $"{Environment.NewLine}Hello, {myName}, on {todaysDate}"
+);
+System.Console.WriteLine(
+  $"{Environment.NewLine}Press any key to exit..."
+);
+System.Console.ReadKey(true);
